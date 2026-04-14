80250a143c2c06933468016bc37f7e921967d6bc
Yash Singh Parihar <yparihar8085@gmail.com>
2026-04-14 11:56:41 +0530

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+  return(
+    <div>
+      hello from user
+    </div>
+  )
+}
+
+export default App;
---
+++ my-app/src/Footer.jsx
@@ -0,0 +1,9 @@
+const yo ="low level";
+function Footer(){
+    return(
+        <div>
+            Footer is here {yo}
+        </div>
+    )
+}
+export default Footer;
